e6520ff8c60a5027e27ad756b15c7b74ab341854
Billy Murphy <william13murphy@gmail.com>
2019-05-07 22:30:12 -0400

    created links/routes for nav

---
+++ src/views/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import EFCNav from '../../components/Nav/EFCNav';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home__container">
+        <EFCNav />
+      </div>
+    );
+  }
+}
+
+export default Home;
