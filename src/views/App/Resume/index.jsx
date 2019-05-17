9ba741fbef12f78d1664b4b5a1284d58aac09d1e
Billy Murphy <william13murphy@gmail.com>
2019-05-17 00:06:50 -0400

    fixed changes that were reverted

--- src/views/App/Resume/index.jsx
+++ src/views/App/Resume/index.jsx
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
+import myResume from './BillyMurphy_Resume_5-3-19.pdf';
+import { Tabs, Tab } from 'react-mdl';
+import './styles.scss';
 
 class Resume extends Component {
+  state = { numPages: null, pageNumber: 1 };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  goToPrevPage = () =>
+    this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
+  goToNextPage = () =>
+    this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+
   render() {
-    return <div>Resume</div>;
+    return (
+      <div className="Resume">
+        <Tabs
+          onChange={tabId => {
+            tabId === 0
+              ? this.setState({ pageNumber: 1 })
+              : this.setState({ pageNumber: 2 });
+          }}
+          ripple
+        >
+          <Tab>Previous</Tab>
+          <Tab>Next</Tab>
+        </Tabs>
+
+        <div style={{ width: '600' }}>
+          <Document
+            className="resume-content"
+            file={myResume}
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            <Page pageNumber={this.state.pageNumber} width={600} />
+          </Document>
+        </div>
+      </div>
+    );
   }
 }
 
